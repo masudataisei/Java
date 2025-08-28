424294a484dcb2b5f33a8164696288947af6f6d1
PC-1 <PC-1@LAPTOP-O1SEODJ1>
2025-08-28 22:36:23 +0900

    ファイル行方不明

--- HelloWorld/src/HelloWorld/HelloWorld.java
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -3,8 +3,21 @@ package HelloWorld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello, World");
-	}
+		//氏名
+		String name = "枡田 大聖";
+		//年齢
+		int age = 23;
+		//趣味
+		String hobby = "散歩";
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
 
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+				
+	}
+			
 }
